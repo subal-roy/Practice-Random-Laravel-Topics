4460417a5b415c97baff9f8907ee96499854f58e
Subal Roy <subal@mediusware.com>
2024-10-15 19:03:16 +0600

    working with event

---
+++ laravel/app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\UserSubscribed;
+use Illuminate\Http\Request;
+
+class NewsletterController extends Controller
+{
+    public function  index() 
+    {
+        return  view('newsletter.index');
+    }
+
+    public function subscribe(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|unique:newsletter|email'
+        ]);
+        
+        event(new UserSubscribed($request->input('email')));
+        
+        return back();
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,5 +8,8 @@ Route::get('/', [ProductController::class, 'index']);
 Route::post('/checkout', [ProductController::class, 'checkout'])->name('checkout');
 Route::get('/success', [ProductController::class, 'success'])->name('checkout.success');
 Route::get('/cancel', [ProductController::class, 'cancel'])->name('checkout.cancel');
+
+Route::get('/newsletter', [NewsletterController::class, 'index']);
+Route::post('/subscribe', [NewsletterController::class, 'subscribe']);
 
 
